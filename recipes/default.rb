9898e6ce940bc0532f73fb02f2fc2db38b6f4cf0
Coder <coderdba@users.noreply.github.com>
2016-10-18 11:57:16 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,14 +9,16 @@
 
 # RUN ORDER
 # First run these on node1
-#users_and_groups.rb
+#users_and_groups.rb --> create one and run if users and groups are not there in the machine
+#setup_ssh.rb
 #directories.rb
 #update_etc_hosts.rb
 #configure_oracleasm.rb
 #prepare_asm_disks.rb
+#Then run grid runinstaller manually (recipe not coded yet)
 
 # Then run these on node2
-#users_and_groups.rb
+#users_and_groups.rb --> create one and run if users and groups are not there in the machine
 #directories.rb
 #update_etc_hosts.rb
 #configure_oracleasm.rb
